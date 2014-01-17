4e4d027adf032e05b5ebf83bb22471753a662f2f
Pete Bachant <petebachant@gmail.com>
2014-01-16 21:39:50 -0500

    Update PerfCurve

--- cactus_py/cactus/cactus.py
+++ cactus_py/cactus/cactus.py
@@ -102,15 +102,64 @@ def runcase(name):
 
 
 class PerfCurve(object):
-    def __init__(self, name, rho=0.002378, vis=1e-6, nr=10):
+    def __init__(self, name):
         self.name = name
-        self.numconfig = config_defaults
-        self.numconfig["nr"] = nr
-        self.caseconfig = case_defaults
-        self.caseconfig["rho"] = rho
-        self.caseconfig["vis"] = vis
-        self.cp = []
+        self.numconfig = {}
+        self.caseconfig = {}
+        # Numerical configuration parameters
+        self.GPFlag = 0
+        self.nr = 10
+        self.nti = 16
+        self.convrg = 0.0001
+        self.iut = 0
+        self.ifc = 0
+        self.ixterm = 0
+        self.ntif = 16
+        self.iutf = 1
+        self.nric = 9
+        self.convrgf = 0.0001
+        self.DiagOutFlag = 1
+        self.Output_ELFlag = 1
+        # Case configuration parameters
+        self.rho = 0.002378
+        self.vis = 0.3739E-6
+        self.tempr = 60.0
+        self.hBLRef = 56.57
+        self.slex = 0.0
+        self.hAG = 30.0
+        self.RPM = 70
+        self.GeomFilePath = "../TestGeom/TestHAWT.geom"
+        self.nSect = 1
+        self.AFDPath = "../../Airfoil_Section_Data/NACA_0015.dat"
+    def setconfig(self):
+        self.numconfig["GPFlag"] = self.GPFlag
+        self.numconfig["nr"] = self.nr
+        self.numconfig["nti"] = self.nti
+        self.numconfig["convrg"] = self.convrg
+        self.numconfig["iut"] = self.iut
+        self.numconfig["ifc"] = self.ifc
+        self.numconfig["ixterm"] = self.ixterm
+        self.numconfig["ntif"] = self.ntif
+        self.numconfig["iutf"] = self.iutf
+        self.numconfig["nric"] = self.nric
+        self.numconfig["convrgf"] = self.convrgf
+        self.numconfig["DiagOutFlag"] = self.DiagOutFlag
+        self.numconfig["Output_ELFlag"] = self.Output_ELFlag
+        self.caseconfig["jbtitle"] = self.name
+        self.caseconfig["rho"] = self.rho
+        self.caseconfig["vis"] = self.vis
+        self.caseconfig["tempr"] = self.tempr
+        self.caseconfig["hBLRef"] = self.hBLRef
+        self.caseconfig["slex"] = self.slex
+        self.caseconfig["hAG"] = self.hAG
+        self.caseconfig["RPM"] = self.RPM
+        self.caseconfig["GeomFilePath"] = self.GeomFilePath
+        self.caseconfig["nSect"] = self.nSect
+        self.caseconfig["AFDPath"] = self.AFDPath
     def run(self, tsr_start, tsr_stop, tsr_step):
+        # Create empty power coefficient list
+        self.cp = []
+        self.setconfig()
         self.tsr = np.arange(tsr_start, tsr_stop+tsr_step, tsr_step)
         for tsr in self.tsr:
             self.caseconfig["Ut"] = tsr
@@ -120,7 +169,9 @@ class PerfCurve(object):
         self.save()
     def save(self):
         with open(self.name+"_PerfCurve.json", "w") as f:
-            data = {"tsr" : self.tsr.tolist(), "cp" : self.cp}
+            data = {"tsr" : self.tsr.tolist(), "cp" : self.cp,
+                    "numconfig" : self.numconfig, 
+                    "caseconfig" : self.caseconfig}
             f.write(json.dumps(data, indent=2))
     def plot(self):
         with open(self.name+"_PerfCurve.json") as f:
@@ -129,12 +180,13 @@ class PerfCurve(object):
         plt.plot(data["tsr"], data["cp"], '-o')
         plt.xlabel(r"$\lambda$")
         plt.ylabel(r"$C_P$")
-		plt.show()
+        plt.show()
     
     
 if __name__ == "__main__":
-    os.chdir("../Test/TestCase1")
+    os.chdir("../../Test/TestCase2")
     plt.close("all")
-    pc = PerfCurve("TestHAWT2")
-#    pc.run(2.0, 6.0, 0.5)
+    pc = PerfCurve("TestVAWT2")
+    pc.GeomFilePath = "../TestGeom/TestVAWT.geom"
+    pc.run(2.0, 4.0, 0.5)
     pc.plot()
