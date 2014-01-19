d83a2214021c5cf1ea0e61e5f0a67fd75f7e874a
Pete Bachant <petebachant@gmail.com>
2014-01-18 20:38:26 -0500

    Fix testvawt.py

--- Test/TestCase2/testvawt.py
+++ Test/TestCase2/testvawt.py
@@ -1,15 +1,14 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jan 16 19:19:55 2014
-
-@author: Pete Bachant
+This script calculates the performance curve of the test VAWT
 """
 
 from cactus import PerfCurve
 
 pc = PerfCurve("TestVAWT2")
-pc.GeomFilePath = "../TestGeom/TestVAWT.geom"
-pc.rho = 1000.0
-pc.vis = 1.0e-6
+pc.basecase = "../TestGeom/TestVAWT.geom"
+pc.basecase.rho = 1000.0
+pc.basecase.viscosity = 1.0e-6
 pc.run(3.0, 7.0, 0.5)
 pc.plot()
