7372693bfa789b0f84f17df487fa23cfa3530ede
Pete Bachant <petebachant@gmail.com>
2014-07-13 10:51:40 -0400

    Change shebang line in test scripts

--- Test/TestCase1/testhawt.py
+++ Test/TestCase1/testhawt.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 This sample script runs a performance curve for the test HAWT case.
--- Test/TestCase2/testvawt.py
+++ Test/TestCase2/testvawt.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 This script calculates the performance curve of the test VAWT
