1cab87fa1206590c6d4aa3d07ccde822b2c025fc
Pete Bachant <petebachant@gmail.com>
2015-01-12 13:14:26 -0500

    Explicitly name foil data file in VAWT Python script

--- Test/TestCase2/testvawt.py
+++ Test/TestCase2/testvawt.py
@@ -8,7 +8,8 @@ from cactus import PerfCurve
 
 pc = PerfCurve("TestVAWT2")
 pc.basecase.geomfile = "../TestGeom/TestVAWT.geom"
+pc.basecase.foil_data_file = "../../Airfoil_Section_Data/NACA_0015.dat"
 pc.basecase.rho = 1000.0
 pc.basecase.viscosity = 1.0e-6
-pc.run(3.0, 7.0, 0.5)
+pc.run(3.0, 7.0, 0.5) # arguments are tsr_start, tsr_stop, tsr_step
 pc.plot()
